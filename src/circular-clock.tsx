82110fbdc7948a3ec1dc5f7106b9c1932edfda99
Aljoscha Meyer <mail@aljoscha-meyer.de>
2023-08-08 09:45:09 +0200

    Introduce clock layout computation

--- src/circular-clock.tsx
+++ src/circular-clock.tsx
@@ -5,6 +5,19 @@ import { Pie, Rotate } from "./svg-utils";
 import { Dial } from "./dial";
 import { sFillSeasonLookup } from "./styles";
 
+// If the radius is this or more, displays the utc time outside the main dial.
+const show_utc_radius = 118;
+
+let hourToString = (n: number): string => {
+  const nToUse = (n + 12) % 24;
+
+  if (nToUse < 10) {
+    return `0${nToUse}`;
+  }
+
+  return `${nToUse}`;
+};
+
 export default function CircularClock({
   width,
   height,
@@ -18,21 +31,82 @@ export default function CircularClock({
   const cx = width / 2;
   const cy = height / 2;
 
-  const highlight_min = radius * 0.72;
-  const highlight_max = radius * 0.98;
+  /* Clock layout, intialized to the smallest circular clock */
+  let main_start = 0.75;
+  let main_width = 0.2;
 
-  const daylightRadius = radius * 0.68;
-  const sun_distance = radius * 0.3;
+  let inner_offset = -0.13; // relative to main_start
+  let inner_width = 0.18;
+
+  let outer_offset = 0; // relative to main_start + main_width
+  let outer_width = 0;
+
+  let highlight_offset_inner = 0.005; // relative to inner_offset
+  let highlight_offset_outer = 0.04; // relative to outer_offset + outer_width
+
+  let daylight_start = 0; // relative to zero
+  let daylight_offset_outer = -0.02; // relative to inner_offset
+
+  let sun_center = (main_start + inner_offset + daylight_offset_outer) * 0.5;
+  let hour_hand_offset = -0.02; // relative to main_start
+
+  let main_scale = 0.7;
+  let inner_scale = 0.6;
+  let outer_scale = 0.6;
+
+  /* Change the layout depending on how much space is available */
+
+  if (radius >= show_utc_radius * 1) {
+    main_start = 0.65;
+    main_width = 0.2;
+    inner_offset = -0.12;
+    inner_width = 0.15;
+    outer_offset = 0.01;
+    outer_width = 0.15;
+    highlight_offset_inner = 0.005;
+    highlight_offset_outer = 0;
+    daylight_offset_outer = -0.02;
+  }
+
+  const main_annulus_start = radius * main_start;
+  const main_annulus_end = main_annulus_start + radius * main_width;
+
+  const inner_annulus_start = radius * (main_start + inner_offset);
+  const inner_annulus_end = inner_annulus_start + radius * inner_width;
+
+  const outer_annulus_start = main_annulus_end + radius * outer_offset;
+  const outer_annulus_end = outer_annulus_start + radius * outer_width;
+
+  const highlight_annulus_start =
+    inner_annulus_start + radius * highlight_offset_inner;
+  const highlight_annulus_end =
+    outer_annulus_end + radius * highlight_offset_outer;
+
+  const daylight_annulus_start = daylight_start;
+  const daylight_annulus_end =
+    inner_annulus_start + radius * daylight_offset_outer;
+
+  const sun_distance = radius * sun_center;
   const sun_radius = radius * 0.035;
-  const hour_hand_tip = radius * 0.73;
-
-  const main_hour_display_min = radius * 0.75;
-  const main_hour_display_max = radius * 0.95;
-  const main_hour_scale = 0.7;
+  const hour_hand_tip = main_annulus_start + radius * hour_hand_offset;
 
   // Convert a fraction of the day to an angle on the face of the clock.
   const time_to_angle = (time: number) => {
     return time * 360 + 0;
+  };
+
+  const determine_highlighting = (hour: number) => {
+    console.log(`r: ${radius}; threshold: ${show_utc_radius}`);
+
+    if (highlighted_hours.has(hour)) {
+      if (radius >= show_utc_radius) {
+        return "sHighlight";
+      } else {
+        return "sHighlightStrongly";
+      }
+    } else {
+      return "";
+    }
   };
 
   let sun_hours_display = <></>;
@@ -58,8 +132,8 @@ export default function CircularClock({
           angle2={
             time_to_angle(sunset) + 0.5 /* background can't shine through*/
           }
-          rMin={0}
-          rMax={daylightRadius}
+          rMin={daylight_annulus_start}
+          rMax={daylight_annulus_end}
           className={"sDay"}
         />
         {/* sunset to civil dusk */}
@@ -68,8 +142,8 @@ export default function CircularClock({
           cy={cy}
           angle1={time_to_angle(sunset)}
           angle2={time_to_angle(sunrise)}
-          rMin={0}
-          rMax={daylightRadius}
+          rMin={daylight_annulus_start}
+          rMax={daylight_annulus_end}
           className={"sCivilDusk"}
         />
         {/* civil dusk to nautical dusk */}
@@ -78,8 +152,8 @@ export default function CircularClock({
           cy={cy}
           angle1={time_to_angle(dusk)}
           angle2={time_to_angle(dawn)}
-          rMin={0}
-          rMax={daylightRadius}
+          rMin={daylight_annulus_start}
+          rMax={daylight_annulus_end}
           className={"sNauticalDusk"}
         />
         {/* nautical dusk through night */}
@@ -88,8 +162,8 @@ export default function CircularClock({
           cy={cy}
           angle1={time_to_angle(nautical_dusk)}
           angle2={time_to_angle(nautical_dawn)}
-          rMin={0}
-          rMax={daylightRadius}
+          rMin={daylight_annulus_start}
+          rMax={daylight_annulus_end}
           className={"sNight"}
         />
       </>
@@ -102,22 +176,20 @@ export default function CircularClock({
       {/* and must be rotated according to the offset to utc */}
       <Rotate cx={cx} cy={cy} angle={time_to_angle(utc_offset)}>
         {sun_hours_display}
-
         {/* highlighted background */}
         <Dial
           cx={cx}
           cy={cy}
-          radMax={highlight_max}
-          radMin={highlight_min}
+          radMax={highlight_annulus_end}
+          radMin={highlight_annulus_start}
           textAlign="center-line"
           ticks={range(24).map((n) => ({
             angle: (360 * n) / 24,
             text: "",
-            className: `sNone ${highlighted_hours.has(n) ? "sHighlight" : ""}`,
+            className: `sNone ${determine_highlighting(n)}`,
             classNameText: ""
           }))}
         />
-
         {/* hour hand */}
         <Rotate cx={cx} cy={cy} angle={time_to_angle(time)}>
           {/* sun */}
@@ -137,33 +209,61 @@ export default function CircularClock({
             className={"sLineInk"}
           />
         </Rotate>
-
         {/* main hour displays */}
         <Dial
           cx={cx}
           cy={cy}
-          radMax={main_hour_display_max}
-          radMin={main_hour_display_min}
+          radMax={main_annulus_end}
+          radMin={main_annulus_start}
           textAlign="center-range"
-          textScale={main_hour_scale}
+          textScale={main_scale}
           dominantBaseline="central"
-          fontSize={(main_hour_display_max - main_hour_display_min) * 0.6}
+          fontSize={(main_annulus_end - main_annulus_start) * 0.6}
           ticks={range(24).map((n) => {
             let hourOf = hourTable[n];
             let moji = `${hourTable[n].emoji}\uFE0F`;
             return {
               angle: (360 * n) / 24,
               text: moji,
-              className: `${sFillSeasonLookup[hourOf.season]} ${
-                highlighted_hours.has(n) ? "sHighlight" : ""
-              }`,
-              classNameText: `sFillInk ${
-                highlighted_hours.has(n) ? "sHighlight" : ""
-              }`
+              className: `${
+                sFillSeasonLookup[hourOf.season]
+              } ${determine_highlighting(n)}`,
+              classNameText: `sFillInk ${determine_highlighting(n)}`
             };
           })}
         />
+        {/* utc time */}
+        <Dial
+          cx={cx}
+          cy={cy}
+          radMax={outer_annulus_end}
+          radMin={outer_annulus_start}
+          textAlign="center-line"
+          textScale={inner_scale}
+          ticks={range(24).map((n) => ({
+            angle: (360 * n) / 24 + 180,
+            text: `U ${("" + n).padStart(2, "0")}`,
+            className: "sNone",
+            classNameText: "sFillInkFaint"
+          }))}
+        />
       </Rotate>
+
+      {/* local time */}
+      <Dial
+        cx={cx}
+        cy={cy}
+        radMax={inner_annulus_end}
+        radMin={inner_annulus_start}
+        textAlign="center-line"
+        textScale={outer_scale}
+        ticks={range(24).map((n) => ({
+          angle: (360 * n) / 24 + 180,
+          text: hourToString(n),
+          className: "sNone",
+          classNameText: "sFillInk"
+        }))}
+      />
     </svg>
   );
 }
