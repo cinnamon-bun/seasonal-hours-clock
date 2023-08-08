fad89f17d1d605e2754dab734a2c1d30efe6c517
Aljoscha Meyer <mail@aljoscha-meyer.de>
2023-08-08 23:18:00 +0200

    Make it compile

--- src/repo-link.tsx
+++ src/repo-link.tsx
@@ -5,13 +5,15 @@ import { ViewportSizeContext } from "./viewport-size-context";
 export default function RepoLink() {
   const { height, width } = useContext(ViewportSizeContext);
 
-  let show_about = Math.abs(height - width) >= 150 || width >= 300;
+  let show_about = Math.abs(height - width) >= 150 || width >= 350;
 
-  return (
-    show_about && (
+  if (show_about) {
+    return (
       <div className="github-link">
         <a href="https://github.com/sgwilym/seasonal-hours-clock">about</a>
       </div>
-    )
-  );
+    );
+  } else {
+    return <></>;
+  }
 }
